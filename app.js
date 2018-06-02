a823fb98019fdf7d23851afecd3df0edadbc6c21
irtzamazhar <iamirtza@gmail.com>
2018-06-03 00:52:18 +0500

    post and put course api

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const Joi = require('joi');
 const app = express();
+
+app.use(express.json());
 
 const courses = [
     { id: 1, name: 'course1' },
@@ -25,6 +28,60 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // If not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The given course is not found.');
+
+    // Validate
+    // If invalid, return 400 = Bad Request
+    const { error } = validateCourse(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    // Update Course
+    // Return the updated course
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The given course is not found.');
+
+    const deleteCourse = courses.indexOf(course);
+    courses.splice(deleteCourse, 1);
+
+    res.send('Course is deleted.');
+});
+
 // PORT
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App is running on port ${port}`));
+
+function validateCourse(course){
+    const Schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, Schema);
+}
